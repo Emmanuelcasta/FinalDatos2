070fbf10060dd3c17740ede8477d81cff20d09b0
Emmanuel Castañeda Cano <ecastanedc@eafit.edu.do>
2024-05-14 20:14:23 -0500

    algoritmo de dijkstra

--- src/TestG.java
+++ src/TestG.java
@@ -1,7 +1,7 @@
 public class TestG {
     public static void main(String [] args) {
         G g = new G();
-        g.readGraph("graph1.txt");
+        g.readGraph("src\\resources\\graphNew.txt");
         g.initializeSingleSource(1);
         g.printGraph();
         g.printVertices();
